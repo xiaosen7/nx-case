fce7349d9d9a4be156ee0564ced3e21c4fedabaf
wangshouren <1115808717@qq.com>
2024-07-07 16:23:45 +0800

    fix: lint

--- apps/app-1/app/page.tsx
+++ apps/app-1/app/page.tsx
@@ -1,14 +1,8 @@
-import { Orders } from '@nx-case-2/orders';
-
 export default function Index() {
   /*
    * Replace the elements below with your own.
    *
    * Note: The corresponding styles are in the ./index.tailwind file.
    */
-  return (
-    <div>
-      <Orders />
-    </div>
-  );
+  return <div></div>;
 }
